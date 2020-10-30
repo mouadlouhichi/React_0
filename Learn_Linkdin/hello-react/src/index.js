e0a986b5cd0e7873617cddb78d99fd10cd97993b
mouadlouhichi <mouadlouhichi@gmail.com>
2020-10-30 19:09:51 +0100

    Test

--- Learn_Linkdin/hello-react/src/index.js
+++ Learn_Linkdin/hello-react/src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
- 
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-   React.createElement("h1", {style: { color:"blue"}}, "Hellooo mouad"),
-  document.getElementById('root')
-);
 
+function GithubUser({login}){
+  const [data,setData] = useState(null);  
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/${login}`)
+    .then( res => res.json())
+    .then(setData)
+    .catch(console.error)
+  }, [])
+
+  if(data){
+    return(
+      <>
+      <h2>{data.login}</h2>
+      <img src={data.avatar_url} width={100} />
+      </>
+    );
+  }
+return null ;
+}
+function App() {
+  return(
+    <GithubUser login="mouadlouhichi"/>
+
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
